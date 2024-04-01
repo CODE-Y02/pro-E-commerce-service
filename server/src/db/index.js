d0bb065d189128e52aed7f30b62f13949acf48bc
yatharth <lakhatey@gmail.com>
2024-04-01 11:07:40 +0530

    Backend: DB setup

---
+++ server/src/db/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const { DB_NAME } = require("../config");
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        autoIndex: true, // Automatically build indexes defined in your schema
+      }
+    );
+
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

520b92a1dd920ccb84985e2ad0617d46051e28e4
code-y02 <yatharthmain@gmail.com>
2024-08-18 15:01:54 +0530

    removed docker

--- server/src/config.js
+++ server/src/config.js
@@ -8,7 +8,12 @@ const NODE_ENV = process.env.NODE_ENV || "local";
 
 // DB config
 const DB_NAME = process.env.DB_NAME || "lifestyle-ecomm";
-const MONGODB_URI = `${process.env.MONGODB_URI}`;
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGO_PASS = process.env.MONGO_PASSWORD || "password";
+
+const MONGODB_URI =
+  process.env.MONGODB_URI ??
+  `mongodb://${process.env.MONGO_USER}:${MONGO_PASS}@localhost:${MONGODB_PORT}`;
 
 // Redis Config
 
